b7d8dad9c2689fbd99885e93ee06023cbac97f89
JyrexRonaldo <65960417+JyrexRonaldo@users.noreply.github.com>
2023-09-21 12:12:10 +0000

    Remove error message class
    Revision of an older version revealed its redundancy

--- js/index.js
+++ js/index.js
@@ -1,16 +1,15 @@
 const pwdInputs = [...document.querySelectorAll("input[type='password']")]
-const errorMessage = document.querySelector('span');
 
 pwdInputs.forEach((input) => {
     input.addEventListener('change', () => {
         if (pwdInputs[0].value !== pwdInputs[1].value) {
-            pwdInputs[0].classList.add("error");
-            pwdInputs[1].classList.add("error");
-            errorMessage.classList.add("error-message");
+            pwdInputs.forEach(inputs => {
+                inputs.classList.add("error");
+            })
         } else if (pwdInputs[0].value === pwdInputs[1].value) {
-            pwdInputs[0].classList.remove("error");
-            pwdInputs[1].classList.remove("error");
-            errorMessage.classList.remove("error-message");
+            pwdInputs.forEach(inputs => {
+                inputs.classList.remove("error");
+            })
         }
     })
 })
